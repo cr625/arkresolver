2a881adf4c3dbbd21cbea56e02005d87badb97dd
cr625 <cr625@drexel.edu>
2021-03-28 20:09:06 -0400

    updated model

--- ark/ark/settings.py
+++ ark/ark/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'resolver.apps.ResolverConfig'
 ]
 
 MIDDLEWARE = [
--- ark/resolver/models.py
+++ ark/resolver/models.py
@@ -1,14 +1,55 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+from django.db.models.deletion import PROTECT
+
 
 class ARK(models.Model):
     ark_id = models.CharField(max_length=200, unique=True)
     shoulder = models.CharField(max_length=10, default='a1')
     NAAN_IDS = [
-        ('13183', 'MRC'),
-        ('87918', 'Library'),
-        ('12345', 'example'),
-        ('99152', 'term'),
-        ('99166', 'agent'),
-        ('99999', 'test'),
+        ('MRC', '13183'),
+        ('Library', '87918'),
+        ('example', '12345'),
+        ('term', '99152'),
+        ('agent', 'agent'),
+        ('test', '99999'),
     ]
-    naan = models.CharField(default='99152', max_length=10, choices=NAAN_IDS)
+    naan = models.CharField(max_length=10, choices=NAAN_IDS, default='MRC')
+    target_uri = models.URLField(blank=True, max_length=255)
+    collection = models.CharField(max_length=200, default='Drexel Web')
+    title = models.CharField(blank=True, max_length=255)
+    notes = models.TextField(blank=True)
+    author = models.ForeignKey(User, default='User', on_delete=PROTECT)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now_add=True)
+
+    class Status(models.IntegerChoices):
+        UNAVAILABLE = 0
+        RESERVED = 1
+        PUBLIC = 2
+        PUBLISHED = 3
+
+    status = models.IntegerField(default=0, choices=Status.choices)
+
+    def __str__(self):
+        return 'ark:/{}/{}{}'.format(self.naan, self.shoulder, self.ark_id)
+
+    class Meta:
+        ordering = ('naan', 'shoulder', 'ark_id')
+
+
+class KernelMetadatum(models.Model):  # https://dublincore.org/groups/kernel/spec/
+    ark = models.ForeignKey(ARK, on_delete=models.CASCADE)
+    # '', about, meta, support, depositor
+    ErcType = models.TextChoices(
+        'ErcType', 'about meta support depositor')
+    erc = models.CharField(blank=True, choices=ErcType.choices, max_length=10)
+    who = models.TextField(blank=True)
+    what = models.TextField(blank=True)
+    when = models.TextField(blank=True)
+    where = models.TextField(blank=True)
+    how = models.TextField(blank=True)
+
+    class Meta:
+        verbose_name_plural = 'KernelMetadata'
